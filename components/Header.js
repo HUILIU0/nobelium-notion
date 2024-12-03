055a5954eee9368741f5e9fd38c36da487077cb1
HUI LIU <semxau@gmail.com>
2024-12-03 17:05:33 +0800

    Update Header.js
    修改知识库链接

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const NavBar = () => {
   const locale = useLocale()
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.PUB, to: 'https://www.thus.chat/', show: true },
+    { id: 1, name: locale.NAV.PUB, to: 'https://liuxiaolin.notion.site/', show: true },
     { id: 2, name: locale.NAV.LINK, to: '/link', show: true },
     { id: 3, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 4, name: locale.NAV.RSS, to: '/feed', show: false, external: true },
